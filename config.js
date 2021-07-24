b07b9eab7bac4860adf8235508d8ae110f845261
Miheo2505 <87906521+Miheo2505@users.noreply.github.com>
2021-07-24 22:45:44 +0700

    Create config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'HÀO ƠIIIIII!',
+    introDesc: `Happy birthday to you
+    Happy birthday to you
+    Happy birthday, happy birthday, happy birthday to you `,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'HAPPY BIRTHDAY 😙',
+    desc: '25/05/1998 ',
+    btnYes: 'Hôm nay ngày bao nhiêu nhỉ?,
+    btnNo: 'Không biết',
+    question:'23 năm trước có một con Hổ được sinh ra đời',
+    btnReply: '<3',
+    reply: 'Chúc m sinh nhật vui vẻ, hạnh phúc',
+    mess: '😘😘',
+    messDesc: 'ĐỪNG ĐÒI ĂN TÔM HÙM NỮA',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://zalp.com' //https://chat.zalo.me/
 }
